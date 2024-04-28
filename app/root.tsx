8f51eb18f9cfe3f788230b00968e7f34fc7c8bb3
Yehudah <you@example.com>
2024-04-28 16:06:09 +0100

    initial commit with new app, migrated data on remix+vite

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+import Nav from "./components/nav";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -14,9 +22,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Arbutus"
+          rel="stylesheet"
+        />
       </head>
       <body>
         {children}
+        <Nav />
         <ScrollRestoration />
         <Scripts />
       </body>
